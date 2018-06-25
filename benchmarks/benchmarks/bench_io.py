d374734e8fb9c0a3af4147409f51df480d2c15f2
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-06-25 16:01:02 -0700

    BENCH: Add benchmarks for np.loadtxt reading from CSV format
    * NumPy currently has no asv benchmarks for np.loadtxt; now adding
    a number of benchmarks for np.loadtxt handling of CSV format files
    such that we have a solid foundation for benchmarking attempted
    performance improvements in CSV file handling

--- benchmarks/benchmarks/bench_io.py
+++ benchmarks/benchmarks/bench_io.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 from .common import Benchmark, get_squares
 
 import numpy as np
+from io import StringIO
 
 
 class Copy(Benchmark):
@@ -62,3 +63,121 @@ class Savez(Benchmark):
 
     def time_vb_savez_squares(self):
         np.savez('tmp.npz', self.squares)
+
+class LoadtxtCSVComments(Benchmark):
+    # benchmarks for np.loadtxt comment handling
+    # when reading in CSV files
+
+    params = [10, int(1e2), int(1e4), int(1e5)]
+    param_names = ['num_lines']
+
+    def setup(self, num_lines):
+        data = ['1,2,3 # comment'] * num_lines
+        self.data_comments = StringIO('\n'.join(data))
+
+    def time_comment_loadtxt_csv(self, num_lines):
+        # benchmark handling of lines with comments
+        # when loading in from csv files
+
+        # inspired by similar benchmark in pandas
+        # for read_csv
+        np.loadtxt(self.data_comments,
+                   delimiter=',')
+
+class LoadtxtCSVdtypes(Benchmark):
+    # benchmarks for np.loadtxt operating with
+    # different dtypes parsed / cast from CSV files
+
+    params = (['float32', 'float64', 'int32', 'int64',
+               'complex128', 'str', 'object'],
+              [10, int(1e2), int(1e4), int(1e5)])
+    param_names = ['dtype', 'num_lines']
+
+    def setup(self, dtype, num_lines):
+        data = ['5, 7, 888'] * num_lines
+        self.csv_data = StringIO('\n'.join(data))
+
+    def time_loadtxt_dtypes_csv(self, dtype, num_lines):
+        # benchmark loading arrays of various dtypes
+        # from csv files
+        np.loadtxt(self.csv_data,
+                   delimiter=',',
+                   dtype=dtype)
+
+class LoadtxtCSVStructured(Benchmark):
+    # benchmarks for np.loadtxt operating with
+    # a structured data type & CSV file
+
+    def setup(self):
+        num_lines = 50000
+        data = ["M, 21, 72, X, 155"] * num_lines
+        self.csv_data = StringIO('\n'.join(data))
+
+    def time_loadtxt_csv_struct_dtype(self):
+        np.loadtxt(self.csv_data,
+                   delimiter=',',
+                   dtype=[('category_1', 'S1'),
+                          ('category_2', 'i4'),
+                          ('category_3', 'f8'),
+                          ('category_4', 'S1'),
+                          ('category_5', 'f8')])
+
+
+class LoadtxtCSVSkipRows(Benchmark):
+    # benchmarks for loadtxt row skipping when
+    # reading in csv file data; a similar benchmark
+    # is present in the pandas asv suite
+
+    params = [0, 500, 10000]
+    param_names = ['skiprows']
+
+    def setup(self, skiprows):
+        np.random.seed(123)
+        test_array = np.random.rand(100000, 3)
+        self.fname = 'test_array.csv'
+        np.savetxt(fname=self.fname,
+                   X=test_array,
+                   delimiter=',')
+
+    def time_skiprows_csv(self, skiprows):
+        np.loadtxt(self.fname,
+                   delimiter=',',
+                   skiprows=skiprows)
+
+class LoadtxtReadUint64Integers(Benchmark):
+    # pandas has a similar CSV reading benchmark
+    # modified to suit np.loadtxt
+
+    params = [550, 1000, 10000]
+    param_names = ['size']
+
+    def setup(self, size):
+        arr = np.arange(size).astype('uint64') + 2**63
+        self.data1 = StringIO('\n'.join(arr.astype(str).tolist()))
+        arr = arr.astype(object)
+        arr[500] = -1
+        self.data2 = StringIO('\n'.join(arr.astype(str).tolist()))
+
+    def time_read_uint64(self, size):
+        np.loadtxt(self.data1)
+
+    def time_read_uint64_neg_values(self, size):
+        np.loadtxt(self.data2)
+
+class LoadtxtUseColsCSV(Benchmark):
+    # benchmark selective column reading from CSV files
+    # using np.loadtxt
+
+    params = [2, [1, 3], [1, 3, 5, 7]]
+    param_names = ['usecols']
+
+    def setup(self, usecols):
+        num_lines = 5000
+        data = ['0, 1, 2, 3, 4, 5, 6, 7, 8, 9'] * num_lines
+        self.csv_data = StringIO('\n'.join(data))
+
+    def time_loadtxt_usecols_csv(self, usecols):
+        np.loadtxt(self.csv_data,
+                   delimiter=',',
+                   usecols=usecols)
+
