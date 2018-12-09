8526a9ec79b8c2f77e9a57896057513aa174b70a
Eric Wieser <wieser.eric@gmail.com>
2018-12-09 01:08:33 -0800

    ENH/DEP: Use a ufunc under the hood for ndarray.clip
    This includes:
    * The addition of 3-input PyObject inner loop
    * The removal of `->f->fastclip` for builtin types, which now use ufuncs instead
    * A deprecation in `PyArray_Clip` for third-party types that still use `->f->fastclip`
    * A deprecation of the unusual casting behavior of `clip`
    * A deprecation of the broken `nan`-behavior of `clip`, which was previously dependent on dimensionality and byte-order.

--- numpy/core/code_generators/generate_umath.py
+++ numpy/core/code_generators/generate_umath.py
@@ -511,6 +511,13 @@ defdict = {
           TD(noobj),
           TD(O, f='npy_ObjectMin')
           ),
+'clip':
+    Ufunc(3, 1, ReorderableNone,
+          docstrings.get('numpy.core.umath.clip'),
+          'PyUFunc_SimpleUniformOperationTypeResolver',
+          TD(noobj),
+          [TypeDescription('O', 'npy_ObjectClip', 'OOO', 'O')]
+          ),
 'fmax':
     Ufunc(2, 1, ReorderableNone,
           docstrings.get('numpy.core.umath.fmax'),
@@ -959,6 +966,9 @@ arity_lookup = {
         'O': 'OO_O',
         'P': 'OO_O_method',
     },
+    (3, 1): {
+        'O': 'OOO_O',
+    }
 }
 
 #for each name
@@ -1135,6 +1145,7 @@ def make_code(funcdict, filename):
     #include "ufunc_type_resolution.h"
     #include "loops.h"
     #include "matmul.h"
+    #include "clip.h"
     %s
 
     static int
